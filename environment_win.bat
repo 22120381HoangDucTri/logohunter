3ee3319f272458e3a864f3bf3f084260c9c4a18c
Hoàng Đức Trí <119548525+22120381HoangDucTri@users.noreply.github.com>
2025-06-02 23:16:38 +0700

    Add files via upload

---
+++ environment_win.bat
@@ -0,0 +1,7 @@
+conda create -n logohunter python=3.6
+conda activate logohunter
+conda install matplotlib keras=2.2.4 pillow scikit-learn
+conda install tensorflow-gpu
+conda install pip
+pip install opencv-python
+pip install pillow
